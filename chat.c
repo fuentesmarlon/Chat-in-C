3896c2923fa6661e2752c1692bce782c4dc162e9
her15177 <her15177@uvg.edu.gt>
2018-10-18 12:37:24 -0600

    Update chat.c

--- chat.c
+++ chat.c
@@ -4,105 +4,99 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
-#include <json/json.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <cjson/cJSON.h>
+#include <json.h>
+
+
+
 
 struct sockaddr_in serv;
+int sock;
+int con = 1;
+char message[100]="";
+
 int fd;
-int conn;
-int numUsers;
-char empty[100] = "";
+struct ifreq ifr;
 
-struct Usuario {
-	int id;
-	char *user;
-	char *status;
-	char *ip;
-};
+int main(int argc, char* argv[]) {
+	
+	char* nickname = argv[1];
+	int cport = atoi(argv[2]);
+	char* sip = argv[3];
+	int sport = atoi(argv[4]);
+	char* cip;
 
-struct Usuario usuarios[5];
 
-int main(int argc, char * argv[]){
-	int port = atoi(argv[0]);
-	numUsers = 0;
-	char message[100] ="";
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	serv.sin_family=AF_INET;
+	serv.sin_port = htons(sport);
 
-	serv.sin_family = AF_INET;
-	serv.sin_port=htons(port);
-	serv.sin_addr.s_addr = INADDR_ANY;
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	bind(fd, (struct sockaddr *)&serv, sizeof(serv));
-	listen(fd,10);
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name, "en0", IFNAMSIZ-1);
+	ioctl(fd, SIOCGIFADDR, &ifr);
+	close(fd);
+	cip = inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+   
+	inet_pton(AF_INET, sip, &serv.sin_addr);
+	connect(sock, (struct sockaddr *)&serv, sizeof(serv));
 
-	while(conn = accept(fd, (struct sockaddr *)NULL, NULL))
-	{
-		printf("esperando");
-		int pid;
-		if((pid=fork())==0){
-			while(recv(conn,message,100,0)>0){
-				printf("Mensaje:%s\n", message);
-				json_object * jobj = json_tokener_parse(message);
-				parseConnection(jobj, conn);				
-				strcpy(message,empty);
-			}
-		exit(0);
+	if(con = 1){
+		printf("INGRESADO COMO USUARIO :%s\n", nickname);
+		printf("SERVER IP :%s\n", sip);
+		printf("CLIENTE IP :%s\n", cip);
+		
+		json_object *juser = json_object_new_object();
+		
+		json_object *host = json_object_new_string(sip);
+		json_object *origin = json_object_new_string(cip);
+		json_object *user = json_object_new_string(nickname);
+
+		json_object_object_add(juser,"action",host);
+		json_object_object_add(juser,"from",origin);
+		json_object_object_add(juser,"to",user);
+
+		char *mes;
+		mes = json_object_to_json_string(juser);
+		printf("%s", mes);
+	}
+
+	int choice=0;
+	while(choice!=4){
+
+		printf("\n\tCLIENT MENU");
+		printf("\n\t------------------------------");
+		printf("\n\n\t 1. CHATEAR CON USUARIOS");
+		printf("\n\t 2. LISTAR USUARIOS");
+		printf("\n\t 3. AYUDA");
+		printf("\n\t 4. SALIR");
+		printf("\n\n INGRESE UNA OPCION: ");
+		scanf("%d", &choice);
+ 
+		switch(choice){
+			case 1:
+			printf("\nCHAT %c",1);
+			break;
+			case 2:
+			printf("\nLISTA DE USUARIOS %c",2);
+			
+			break;
+			case 3:
+			printf("\nAYUDA %c",3);
+			break;
+			case 4:
+			printf("\nSALIR %c",4);
+			exit(0);
+			otherwise:
+			printf("\nOPCION INVALIDA");
 		}
 	}
-	
-}
-
-int parseConnection(json_object * jobj, int socket){
-	
-	json_object* origin;
-	json_object* nombre;
-
-	json_object_object_get_ex(jobj,"origin",&origin);
-	const char *originS = json_object_get_string(origin);
-	json_object_object_get_ex(jobj,"nombre",&nombre);
-	const char *nombreS = json_object_get_string(nombre);
-
-	int c;
-	int yasta = 0;
-	if(numUsers > 0){
-		for(c=0;c<5;c++){
-			if(strcmp(nombreS,usuarios[c].user) == 0){
-				yasta = 1;		
-				break;
-			}	
-		}
-	}
-	if(yasta){
-		sendMensajeError(originS, socket);
-	}
-	else{
-		agregarUsuario(originS, nombreS);
-	}
-}
-
-int sendMensajeError(char *origin, int socket){
-	char *mensaje = "{\"status\": \"ERROR\", \"message\": \"Usuario ingresado ya existe.\"}";
-	send(socket, mensaje, 100, 0);
-}
-
-int agregarUsuario(char *origin, char *nombre, int socket){
-	usuarios[numUsers].id = numUsers;
-	usuarios[numUsers].user = nombre;
-	usuarios[numUsers].status = "active";
-	usuarios[numUsers].ip = origin;
-	char *idS;
-	sprintf(idS, "%d", numUsers);
-	struct json_object *id = json_object_new_string(idS);
-	struct json_object *name = json_object_new_string(nombre);
-	struct json_object *status = json_object_new_string("activo");
-	struct json_object *user = json_object_new_object();
-	json_object_object_add(user,"id",id);
-	json_object_object_add(user,"name",name);	
-	json_object_object_add(user,"status",status);
-	struct json_object *reply = json_object_new_object();
-	struct json_object *userConnected = json_object_new_string("USER_CONNECTED");
-	json_object_object_add(reply,"action",userConnected);
-	json_object_object_add(reply,"user",user);
-	send(socket, json_object_get_string(reply),100,0);
-	numUsers++;
 }
