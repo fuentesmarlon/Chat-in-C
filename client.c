8b8f43093a15144316305f3fe20313448283cb5e
her15177 <her15177@uvg.edu.gt>
2018-10-25 12:39:04 -0600

    funciona todo menos list porque no puedo probar

--- client.c
+++ client.c
@@ -12,8 +12,10 @@
 #include <netdb.h>
 #include <ifaddrs.h>
 #include <json/json.h>
+#include <pthread.h>
 
 
+pthread_t thread;
 struct sockaddr_in serv;
 int sock;
 int conn, con;
@@ -23,6 +25,62 @@ int fd;
 struct ifreq ifr;
 char empty[100] = "";
 
+void * listening(){
+	char meServer[1024];
+	int resp;
+	while(1){
+		bzero(meServer, 1024);
+		resp = recv(fd,meServer,1024,0);
+		if(resp<=0){
+			
+		}else{
+			json_object *jChecker = json_tokener_parse(meServer);
+			json_object *action;
+			json_object_object_get_ex(jChecker,"action",&action);
+			const char *accion = json_object_get_string(action);
+			
+			if(accion=="RECEIVE_MESSAGE"){
+				json_object *from;
+				json_object *messageJ;
+				json_object *jReceive =json_tokener_parse(meServer);
+				json_object_object_get_ex(jReceive,"from",&from);
+				json_object_object_get_ex(jReceive,"message",&messageJ);
+				const char *fromName = json_object_get_string(from);
+				const char *meString = json_object_get_string(messageJ);
+				printf("MENSAJE DE: %s\n",fromName);
+				printf("MENSAJE: %s\n",meString);		
+
+			}if(accion=="LIST_USER"){
+				json_object *users;
+				json_object *jList = json_tokener_parse(meServer);
+				json_object_object_get_ex(jList,"users",&users);
+				const char *listUsers = json_object_get_string(jList);
+				printf("LISTA DE USUARIOS: %s",listUsers);
+
+			}if(accion=="CHANGED_STATUS"){
+				json_object *user;
+				json_object *jStatus = json_tokener_parse(meServer);
+				json_object_object_get_ex(jStatus,"user",&user);
+				const char *changedStatus = json_object_get_string(jStatus);
+				printf("STATUS CAMBIO A: %s",changedStatus);
+
+			}if(accion=="USER_DISCONNECTED"){
+				json_object *user;
+				json_object *jStatus = json_tokener_parse(meServer);
+				json_object_object_get_ex(jStatus,"user",&user);
+				const char *changedStatus = json_object_get_string(jStatus);
+				printf("STATUS CAMBIO A: %s",changedStatus);
+				
+
+			}else{
+				printf("ACTION ERROR");
+			}
+
+		}
+
+	}
+}
+
 int main(int argc, char* argv[]) {
 	
 	char* nickname = argv[1];
@@ -30,6 +88,7 @@ int main(int argc, char* argv[]) {
 	char* sip = argv[3];
 	int sport = atoi(argv[4]);
 	char* cip;
+	const char *id;
 
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -47,9 +106,9 @@ int main(int argc, char* argv[]) {
 	inet_pton(AF_INET, sip, &serv.sin_addr);
 	
 	if(connect(sock, (struct sockaddr *)&serv, sizeof(serv)) == 0){
-		printf("INGRESADO COMO USUARIO: %s\n", nickname);
+		printf("INGRESADO COMO USUARIO: %s\n\n", nickname);
 		printf("SERVER IP: %s\n", sip);
-		printf("CLIENTE IP: %s\n", cip);
+		printf("CLIENTE IP: %s\n\n", cip);
 
 
 		json_object *juser = json_object_new_object();
@@ -70,20 +129,43 @@ int main(int argc, char* argv[]) {
 		bzero(message, 100);
 		while(handshake == 0){
 			recv(sock, message, sizeof(message),0);
-			printf("ESPERANDO CONEXION\n");
-			printf("HANDSHAKE:%s\n", message);
-			printf("CONEXION EXITOSA\n");
+			printf("ESPERANDO CONEXION\n\n");
+			printf("HANDSHAKE ");
+			
+			json_object *jhand = json_tokener_parse(message);
+			json_object *jshake;
+			json_object *shake;
+			json_object *usrid;
+			json_object *usrname;
+			json_object *usrstatus;
+			json_object_object_get_ex(jhand,"status",&shake);
+			json_object_object_get_ex(jhand,"user",&jshake);
+			json_object_object_get_ex(jshake,"id",&usrid);
+			json_object_object_get_ex(jshake,"name",&usrname);
+			json_object_object_get_ex(jshake,"status",&usrstatus);
+			const char *ok = json_object_get_string(shake);
+			id = json_object_get_string(usrid);
+			const char *uname = json_object_get_string(usrname);
+			const char *ust = json_object_get_string(usrstatus);
+
+			printf("STATUS %s\n\n", ok);
+			printf("ID: %s\n", id);
+			printf("USER: %s\n", uname);
+			printf("STATUS: %s\n", ust);
+			printf("\nCONEXION EXITOSA\n");
+			
 			if(message>0){
 				handshake=1;
 			}
 			bzero(message, 100);
 			}
 	}else{
-		printf("CONEXION FALLIDA\n");
+		printf("CONEXION FALLIDA\n\n");
 		exit(0);
 	}
 	
 	int choice=0;
+	pthread_create(&thread,NULL,listening,(void*)&sock);
 	while(choice!=5){
 
 		printf("\n\tCLIENT MENU");
@@ -129,16 +211,17 @@ int main(int argc, char* argv[]) {
 			json_object *jAction2 = json_object_new_string("LIST_USER");
 			json_object_object_add(jlist,"action",jAction2);
 			const char *listar;
-
+			
 			listar = json_object_to_json_string(jlist);
 			send(sock, listar, strlen(listar),0);
 			
 			handshake = 0;
 			bzero(message, 100);
 			while(handshake == 0){
-				printf("ESPERANDO LISTA...");
+				printf("\nESPERANDO LISTA...\n");
 				recv(sock, message, sizeof(message),0);
 				printf("LISTA: %s\n", message);
+				
 				if(message>0){
 					handshake=1;
 				}
@@ -148,17 +231,49 @@ int main(int argc, char* argv[]) {
 			printf("\nBUSCAR USUARIO \n",2);
 			json_object *jbus = json_object_new_object();
 			char user[50];
-			printf("INGRESE USUARIO: ");
+			printf("INGRESE ID DE USUARIO: ");
 			scanf("%s",user);
 
-			json_object *jUser = json_object_new_string(nickname);
+			json_object *jUser = json_object_new_string(user);
 
 			json_object_object_add(jbus,"action",jAction2);
-			json_object_object_add(jbus,"from",jUser);
+			json_object_object_add(jbus,"user",jUser);
 
 			const char *bus;
 			bus = json_object_to_json_string(jbus);
-			send(sock, bus, strlen(bus),0);
+			send(sock, bus, sizeof(bus),0);
+			
+			handshake = 0;
+			bzero(message, 100);
+			while(handshake == 0){
+				printf("ESPERANDO USUARIO...\n");
+				recv(sock, message, strlen(message),0);
+				printf("^^ %s\n", message);
+				json_object *j1users = json_tokener_parse(message);
+				json_object *j1user;
+				json_object *j1id;
+				json_object *j1name;
+				json_object *j1status;
+				json_object_object_get_ex(j1users,"users",&j1user);
+				json_object_object_get_ex(j1user,"id",&j1id);
+				json_object_object_get_ex(j1user,"name",&j1name);
+				json_object_object_get_ex(j1user,"status",&j1status);
+				const char *id1 = json_object_get_string(j1id);
+				const char *name1 = json_object_get_string(j1name);
+				const char *status1 = json_object_get_string(j1status);
+				
+				
+				printf("INFO USUARIO: %s\n", name1);
+				printf("ID: %s\n", id1);
+				printf("STATUS: %s\n", status1);
+				printf("\n\n");
+				
+				if(message>0){
+					handshake=1;
+				}
+				bzero(message, 100);
+			}
+			
 			break;
 			
 			case 3:
@@ -166,7 +281,7 @@ int main(int argc, char* argv[]) {
 			
 			json_object *jstatus = json_object_new_object();
 			json_object *jAction3 = json_object_new_string("CHANGE_STATUS");
-			json_object *jId = json_object_new_string(nickname);
+			json_object *jId = json_object_new_string(id);
 			
 			int status=0;
 			char *st = "active";
@@ -209,11 +324,17 @@ int main(int argc, char* argv[]) {
 			const char *change;
 			change = json_object_to_json_string(jstatus);
 			send(sock, change, strlen(change),0);
-			
 			break;
 			
 			case 4:
 			printf("\nAYUDA \n",4);
+			printf("\n\t------------------------------");
+			printf("\n\n\t 1. CHATEAR CON USUARIOS CONECTADOS");
+			printf("\n\t 2. LISTAR USUARIOS CONECTADOS");
+			printf("\n\t 3. CAMBIAR STATUS DE USUARIO");
+			printf("\n\t 4. VER ESTA AYUDA");
+			printf("\n\t 5. SALIR DEL CHAT ");
+			printf("\n\t------------------------------\n\n\n");
 			break;
 			
 			case 5:
@@ -241,6 +362,5 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 	}
+
 }
-
-
